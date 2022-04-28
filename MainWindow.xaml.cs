0ac500840b34826bc0222aec3cf9d5a42d388c09
Андрей Беззубов <it@oink.ru>
2022-04-28 10:43:47 +0500

    Реализовазала генерацию кода и авторизацию!

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,55 @@ namespace TeleCom
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+        //Константное значение символов для генерации кода
+        public static readonly string[] symbols = {"1", "2", "3", "4", "5", "6", "A", "b"};
+        //Метод генерации одноразового кода
+        public static string Generate_Code(int length)
+        {
+            string s = "";
+            Random rand = new Random();
+            for (int i = 0; i < length; i++)
+            {
+                s += symbols[rand.Next(0, 7)];
+            }
+            return s;
+        }
+        //Сам вход в программу
+        private void Btvhod_Click(object sender, RoutedEventArgs e)
+        {
+            if (Tbkod.Text == code && is_login_timeout != false) MessageBox.Show("Успешная авторизация!");
+        }
+
+        //Ввод номера
+        private void TbNomer_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (new BD.DemoBKEntities().ТехПерсонал.FirstOrDefault(i => i.Номер == TbNomer.Text.Trim()) != null) Tbpassword.IsEnabled = true;
+           
+        }
+
+        //Ввод пароля
+        public static string code = "";
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (new BD.DemoBKEntities().ТехПерсонал.FirstOrDefault(i => i.Пароль == Tbpassword.Text.Trim()) != null && e.Key == Key.Enter)
+            {
+                code = Generate_Code(8);
+                MessageBox.Show(code);
+                Async_Method();
+            } 
+        }
+
+        //поле для хранения логического значения
+        public static bool is_login_timeout = true;
+        private async void Async_Method()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+               await Task.Delay(1000);
+            }
+            is_login_timeout = false;
         }
     }
 }
